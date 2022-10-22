07b08dfa3b4071c37bbbe14c10962e54e17122bf
JuliaNiky <yuliia.nikolaienko.1609@gmail.com>
2022-10-22 23:31:42 +0300

    Create a gallery with full-size img
    Create a gallery with the ability to click on its elements and view the full-size image in a modal window.

---
+++ js/01-gallery.js
@@ -0,0 +1,63 @@
+import { galleryItems } from "./gallery-items.js";
+
+// Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const imgMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", imgMarkup);
+galleryRef.addEventListener("click", onImgZoomClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `;
+    })
+    .join(" ");
+}
+
+function onImgZoomClick(event) {
+  const isImg = event.target.classList.contains("gallery__image");
+
+  if (!isImg) {
+    return;
+  }
+
+  zoomImg(event);
+}
+
+function zoomImg(event) {
+  event.preventDefault();
+  const imgZoomLink = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+        <img src="${imgZoomLink}" width="1280">
+    `,
+    {
+      onClose: () => {
+        window.removeEventListener("keydown", closeImgZoomClick);
+      },
+    }
+  );
+
+  window.addEventListener("keydown", closeImgZoomClick);
+
+  instance.show();
+
+  function closeImgZoomClick(event) {
+    console.log(event.code);
+    if (event.code == "Escape") {
+      instance.close();
+    }
+  }
+}
